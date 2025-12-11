e20f027be6c1876809ffaa5f8ddab739281ebcc8
esgosmo <julianjaquez@gmail.com>
2025-12-11 12:20:33 -0600

    Ya vuelven a ser dependientes los labels

--- script.js
+++ script.js
@@ -853,7 +853,7 @@ function draw() {
                 if (isTightHoriz && showAspect) { ctx.textAlign = "left"; ctx.fillText(txtRes, offsetX + padding, offsetY + padding + lineHeight); } 
                 else { ctx.textAlign = showAspect ? "right" : "left"; const posX = showAspect ? (offsetX + visibleW - padding) : (offsetX + padding); ctx.fillText(txtRes, posX, offsetY + padding); }
             }
-        }
+       //}
 
         // --- B. DIBUJAR SECONDARY FRAMELINE TEXT ---
         if (drawSec && inputs.secAspect) {
