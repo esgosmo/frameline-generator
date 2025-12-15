53bbbe48f0b7c5acb6a0f72c791ed5ae640cf535
esgosmo <julianjaquez@gmail.com>
2025-12-14 20:59:33 -0600

    correcciones dropdown

--- script.js
+++ script.js
@@ -123,7 +123,7 @@ function renderResolutionMenu() {
             optgroup.label = nombre;
             
             // Regla: Broadcast y DCI muestran todo. El resto solo Top 3.
-            const mostrarTodo = nombre.includes("Broadcast") || nombre.includes("DCI");
+            const mostrarTodo = nombre.includes("Broadcast") || nombre.includes("DCI") || nombre.includes("Social Media");
             
             let itemsAMostrar = items;
             let hayBotonVerMas = false;
